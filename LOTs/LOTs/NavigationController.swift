b38581ee730815c8140c98f158fec9081390b80c
niafan0128@gmail.com <niafan0128@gmail.com>
2018-09-19 12:15:03 +0800

    Complete the tab layout of Main Page

---
+++ LOTs/LOTs/NavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  NavigationController.swift
+//  LOTs
+//
+//  Created by 乃方 on 2018/9/19.
+//  Copyright © 2018年 Nia. All rights reserved.
+//
+
+import Foundation
+
+//class NavigationController: UINavigationController {
+//
+//
+//}
---
+++ LOTs/LOTs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LOTs
+//
+//  Created by 乃方 on 2018/9/19.
+//  Copyright © 2018年 Nia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
