3749af499adf508eafc0cec5275ab679502309c5
niafan0128@gmail.com <niafan0128@gmail.com>
2018-09-29 00:48:36 +0800

    Apply the customized layout of Suggest Page.

--- LOTs/SuggestViewController copy.swift
+++ LOTs/SuggestViewController copy.swift
@@ -13,10 +13,7 @@ class SuggestViewController: UIViewController {
     @IBOutlet weak var showCollectionView: UICollectionView!
     
     var fullScreenSize: CGSize!
-    
-//    var numberOfColumns = 3
-//    var cellPadding: CGFloat = 6
-//    var cache = [UICollectionViewLayoutAttributes]()
+    var articleImage = [UIImage]()
     var photoWidth: CGFloat!
     
     override func viewDidLoad() {
@@ -24,7 +21,6 @@ class SuggestViewController: UIViewController {
         super.viewDidLoad()
         
         fullScreenSize = UIScreen.main.bounds.size
-        print(fullScreenSize)
         typeCollectionSet()
         
         let nib2 = UINib(nibName: "ProfileCollectionViewCell", bundle: nil)
@@ -39,8 +35,21 @@ class SuggestViewController: UIViewController {
 //        layout2.itemSize = CGSize(width: photoWidth, height: photoWidth)
 //        showCollectionView.collectionViewLayout = layout2
         
+        articleImage = [UIImage(named: "01"), UIImage(named: "02"),
+                        UIImage(named: "03"), UIImage(named: "04"),
+                        UIImage(named: "05"), UIImage(named: "06"),
+                        UIImage(named: "07"), UIImage(named: "08"),
+                        UIImage(named: "09"), UIImage(named: "10"),
+                        UIImage(named: "01"), UIImage(named: "02"),
+                        UIImage(named: "03"), UIImage(named: "04"),
+                        UIImage(named: "05"), UIImage(named: "06"),
+                        UIImage(named: "07"), UIImage(named: "08")] as! [UIImage]
+        
         showCollectionView.delegate = self
         showCollectionView.dataSource = self
+        
+//        self.showCollectionView!.collectionViewLayout = circularLayoutObject
+
         
     }
     
@@ -74,7 +83,7 @@ extension SuggestViewController: UICollectionViewDataSource {
             
         } else {
             
-            return 6
+            return 18
             
         }
         
@@ -100,11 +109,14 @@ extension SuggestViewController: UICollectionViewDataSource {
                 
             }
             
+            cell.articleImage.image = articleImage[indexPath.item]
+            
             return cell
             
         }
         
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
@@ -112,117 +124,68 @@ extension SuggestViewController: UICollectionViewDataSource {
         
     }
     
-    
 }
 
 extension SuggestViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
+
         let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-        
+
         if collectionView == self.showCollectionView {
-            
+
             if indexPath.item % 6 == 0 {
-                
-                let cellWidth = (fullScreenSize.width / 3) - 7.5
-                
-                print(cellWidth)
-                print(flowLayout.minimumLineSpacing)
-                print(flowLayout.minimumInteritemSpacing)
-                return CGSize(width: cellWidth, height: cellWidth)
-                
-            } else if indexPath.item % 6 == 1 {
-                
-                let cellWidth = (fullScreenSize.width / 3) - 7.5
-                
-                return CGSize(width: cellWidth, height: cellWidth)
-                
-            } else if indexPath.item % 6 == 2 {
-                
-                let cellWidth = (fullScreenSize.width / 3) - 7.5
-                
-                return CGSize(width: cellWidth, height: cellWidth)
-                
-            } else if indexPath.item % 6 == 3 {
-                
-                let cellWidth = ((fullScreenSize.width / 3) - 10) * 2 + 15
-                print(cellWidth)
-                return CGSize(width: cellWidth, height: cellWidth)
-                
-            } else if indexPath.item % 6 == 4 {
-                
+
+//                let cellWidth = (fullScreenSize.width / 3) - 7.5
+//
+//                print(cellWidth)
+//
+//                return CGSize(width: cellWidth, height: cellWidth)
+//
+//            } else if indexPath.item % 6 == 1 {
+//
+//                let cellWidth = (fullScreenSize.width / 3) - 7.5
+//
+//                return CGSize(width: cellWidth, height: cellWidth)
+//
+//            } else if indexPath.item % 6 == 2 {
+//
+//                let cellWidth = (fullScreenSize.width / 3) - 7.5
+//
+//                return CGSize(width: cellWidth, height: cellWidth)
+//
+//            } else if indexPath.item % 6 == 3 {
+//
+//                let cellWidth = ((fullScreenSize.width / 3) - 10) * 2 + 15
+//
+//                return CGSize(width: cellWidth, height: cellWidth)
+//
+//            } else if indexPath.item % 6 == 4 {
+//
+//                let cellWidth = (fullScreenSize.width / 3) - 7.5
+//
+//                return CGSize(width: cellWidth, height: cellWidth)
+//
+//            } else if indexPath.item % 6 == 5 {
+//
                 let cellWidth = (fullScreenSize.width / 3) - 7.5
 
                 return CGSize(width: cellWidth, height: cellWidth)
-                
-            } else if indexPath.item % 6 == 5 {
-                
-                let cellWidth = (fullScreenSize.width / 3) - 7.5
-                
-                return CGSize(width: cellWidth, height: cellWidth)
-                
+
             }
             
-//            if indexPath.item % 6 == 0 {
-//
-//                let cellWidth = (fullScreenSize.width / 3) - 10
-//
-//                return CGSize(width: cellWidth, height: cellWidth)
-//
-//            } else {
-//
-//                let cellWidth = ((fullScreenSize.width / 3) - 10) * 2
-//
-//                return CGSize(width: cellWidth, height: cellWidth)
-//
-//            }
-            
         }
-        
+
         return CGSize(width: (self.view.frame.size.width - 30) / 2, height: 80)
     }
-    
+
 }
 
-
-//if collectionView == self.showCollectionView {
-//
-//    let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-//
-//    var numberOfCellsPerLine = 0
-//
-//    if indexPath.item % 6 == 0 {
-//
-//        numberOfCellsPerLine = 3
-//
-//    } else if indexPath.item % 6 == 1 {
-//
-//        numberOfCellsPerLine = 3
-//
-//    } else if indexPath.item % 6 == 2 {
-//
-//        numberOfCellsPerLine = 3
-//
-//    } else if indexPath.item % 6 == 3 {
-//
-//        numberOfCellsPerLine = 1
-//
-//    } else if indexPath.item % 6 == 4 {
-//
-//        numberOfCellsPerLine = 3
-//
-//    } else if indexPath.item % 6 == 5 {
-//
-//        numberOfCellsPerLine = 3
-//
-//    }
-//
 //    let cellWidth = (collectionView.bounds.width - (flowLayout.sectionInset.left + flowLayout.sectionInset.right) - flowLayout.minimumInteritemSpacing * CGFloat(numberOfCellsPerLine - 1)) / CGFloat(numberOfCellsPerLine)
 //
 //    return CGSize(width: cellWidth, height: cellWidth)
 //
 //}
 //
-////                layout.itemSize = CGSize(width: (self.view.frame.size.width - 30) / 2, height: 80)
+//// layout.itemSize = CGSize(width: (self.view.frame.size.width - 30) / 2, height: 80)
 //return CGSize(width: (self.view.frame.size.width - 30) / 2, height: 80)
