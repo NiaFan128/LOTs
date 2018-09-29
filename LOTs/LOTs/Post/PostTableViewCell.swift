89b032cdfe067ef3d54334b46400ddc60d55e302
niafan0128@gmail.com <niafan0128@gmail.com>
2018-09-29 11:09:02 +0800

    Modify the PickerView of Post Page with MCPicker.

--- LOTs/LOTs/Post/PostTableViewCell.swift
+++ LOTs/LOTs/Post/PostTableViewCell.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
-import iOSDropDown
+import McPicker
+//import iOSDropDown
 import DatePickerDialog
 
 class PostTableViewCell: UITableViewCell {
 
     @IBOutlet weak var infoView: UIView!
     @IBOutlet weak var dateTextField: UITextField!
-    @IBOutlet weak var locationTextField: DropDown!
-    @IBOutlet weak var cuisineTextField: DropDown!
+    @IBOutlet weak var locationTextField: McTextField!
+    @IBOutlet weak var cuisineTextField: McTextField!
+//    @IBOutlet weak var cuisineTextField: DropDown!
     @IBOutlet weak var instagramSwitchButton: UIButton!
     
     var locationCompletion: ((_ data: String) -> Void)?
@@ -33,12 +35,10 @@ class PostTableViewCell: UITableViewCell {
         super.awakeFromNib()
         
         self.infoBackgroundSet()
-        self.locationPick()
-        self.cuisinePick()
+        self.locationPicker()
+        self.cuisinePicker()
         self.date()
-        
-        locationTextField.target(forAction: #selector(locationTextField.showList), withSender: self)
-        
+    
         dateTextField.delegate = self
 
     }
@@ -65,43 +65,101 @@ class PostTableViewCell: UITableViewCell {
         
     }
     
-    func locationPick(){
+    func locationPicker(){
         
+        let data: [[String]] = [
+            ["中正區", "大同區", "中山區", "松山區", "大安區", "萬華區",
+             "信義區", "士林區", "北投區", "內湖區", "南港區", "文山區"]
+        ]
         
-        locationTextField.selectedRowColor = .white
+        let locationInputView = McPicker(data: data)
+        
+        // 彈跳起來的上方背景色
+        locationInputView.backgroundColor = .gray
+        locationInputView.backgroundColorAlpha = 0.25
+        locationTextField.inputViewMcPicker = locationInputView
+        
+        locationTextField.doneHandler = { [weak locationTextField] (selections) in
+            
+            self.selectLocation = selections[0]!
+            
+            locationTextField?.text = self.selectLocation
+            locationTextField?.textColor = UIColor(red: 211.0/255.0, green: 90.0/255.0, blue: 102.0/255.0, alpha: 1.0)
+            self.locationCompletion?(self.selectLocation ?? "")
+            
+        }
+        
+        locationTextField.selectionChangedHandler = { [weak locationTextField] (selections, componentThatChanged) in
+            
+            locationTextField?.text = selections[componentThatChanged]!
+            
+        }
+        
+        locationTextField.cancelHandler = { [weak locationTextField] in
+            
+            locationTextField?.text = "Cancelled."
+            
+        }
+        
+        locationTextField.textFieldWillBeginEditingHandler = { [weak locationTextField] (selections) in
+            
+            if locationTextField?.text == "" {
+                // Selections always default to the first value per component
+                locationTextField?.text = selections[0]
+                
+            }
+            
+        }
 
-        locationTextField.optionArray = ["大安區", "信義區", "中山區", "內湖區"]
-        locationTextField.optionIds = [1, 23, 54, 22]
+    }
+    
+    func cuisinePicker(){
         
-        locationTextField.didSelect { (selectedText, index, id) in
+        let data: [[String]] = [
+            ["日式料理", "中式料理", "韓式料理", "台式料理", "美式料理", "義式料理"]
+        ]
+        
+        let mcInputView = McPicker(data: data)
+        
+        // 彈跳起來的上方背景色
+        mcInputView.backgroundColor = .gray
+        mcInputView.backgroundColorAlpha = 0.25
+        cuisineTextField.inputViewMcPicker = mcInputView
+        
+        cuisineTextField.doneHandler = { [weak cuisineTextField] (selections) in
             
-            self.locationCompletion?(selectedText)
+            self.selectCuisine = selections[0]!
             
-            self.locationTextField.text = "\(selectedText)"
-            self.locationTextField.textColor = UIColor(red: 211.0/255.0, green: 90.0/255.0, blue: 102.0/255.0, alpha: 1.0)
+            cuisineTextField?.text = self.selectCuisine
+            cuisineTextField?.textColor = UIColor(red: 211.0/255.0, green: 90.0/255.0, blue: 102.0/255.0, alpha: 1.0)
+
+            self.cuisineCompletion?(self.selectCuisine ?? "")
+
+        }
+        
+        cuisineTextField.selectionChangedHandler = { [weak cuisineTextField] (selections, componentThatChanged) in
+
+            cuisineTextField?.text = selections[componentThatChanged]!
+
+        }
+
+        cuisineTextField.cancelHandler = { [weak cuisineTextField] in
+            
+            cuisineTextField?.text = "Cancelled."
+        
+        }
+        
+        cuisineTextField.textFieldWillBeginEditingHandler = { [weak cuisineTextField] (selections) in
+            
+            if cuisineTextField?.text == "" {
+                // Selections always default to the first value per component
+                cuisineTextField?.text = selections[0]
+            
+            }
             
         }
         
     }
-    
-    func cuisinePick() {
-        
-        cuisineTextField.selectedRowColor = .white
-        
-        cuisineTextField.optionArray = ["中式料理", "日式料理", "韓式料理", "美式料理"]
-        cuisineTextField.optionIds = [1, 23, 54, 22]
-        
-        cuisineTextField.didSelect { (selectedText, index, id) in
-            
-            self.cuisineCompletion?(selectedText)
-
-            self.cuisineTextField.text = "\(selectedText)"
-            self.cuisineTextField.textColor = UIColor(red: 211.0/255.0, green: 90.0/255.0, blue: 102.0/255.0, alpha: 1.0)
-                        
-        }
-        
-    }
-    
     
     @IBAction func instagramSelect(_ sender: Any) {
         
--- LOTs/LOTs/ViewController.swift
+++ LOTs/LOTs/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import McPicker
 
-class SearchViewController: UIViewController {
+class ViewController: UIViewController {
 
+    @IBOutlet weak var mcTextField: McTextField!
+    
+    let data: [[String]] = [
+        ["日式料理", "中式料理", "韓式料理", "台式料理", "美式料理", "義式料理"]
+    ]
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let mcInputView = McPicker(data: data)
+
+        // 彈跳起來的上方背景色
+        mcInputView.backgroundColor = .gray
+        mcInputView.backgroundColorAlpha = 0.25
+        mcTextField.inputViewMcPicker = mcInputView
+        
+        mcTextField.doneHandler = { [weak mcTextField] (selections) in
+            mcTextField?.text = selections[0]!
+            print(mcTextField?.text)
+        }
+        
+        mcTextField.selectionChangedHandler = { [weak mcTextField] (selections, componentThatChanged) in
+            mcTextField?.text = selections[componentThatChanged]!
+            print(mcTextField?.text)
+        }
+        mcTextField.cancelHandler = { [weak mcTextField] in
+            mcTextField?.text = "Cancelled."
+        }
+        mcTextField.textFieldWillBeginEditingHandler = { [weak mcTextField] (selections) in
+            if mcTextField?.text == "" {
+                // Selections always default to the first value per component
+                mcTextField?.text = selections[0]
+            }
+            
+        }
+        
     }
-
-
+    
 }
-
