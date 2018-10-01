350255f4ec56264311898a6b1e5059ecbcfa086c
niafan0128@gmail.com <niafan0128@gmail.com>
2018-10-02 00:43:16 +0800

    Adjust the layout of Login Page.

--- LOTs/LOTs/Controller/Login/LoginViewController.swift
+++ LOTs/LOTs/Controller/Login/LoginViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
 import FBSDKLoginKit
 import FBSDKCoreKit
 
@@ -15,7 +17,7 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var facebookLoginButton: UIButton!
     @IBOutlet weak var googleLoginButton: UIButton!
-    @IBOutlet weak var GuestModeButton: UIButton!
+    @IBOutlet weak var guestModeButton: UIButton!
     @IBOutlet weak var facebookImage: UIImageView!
     @IBOutlet weak var googleImage: UIImageView!
     
@@ -23,16 +25,17 @@ class LoginViewController: UIViewController {
         
         super.viewDidLoad()
 
-        facebookLoginButton.layer.cornerRadius = 7
-        googleLoginButton.layer.cornerRadius = 7
-        GuestModeButton.layer.cornerRadius = 7
+        facebookLoginButton.corner(corner: 7)
+        guestModeButton.corner(corner: 7)
+        
+        googleLoginButton.isHidden = true
+        googleImage.isHidden = true
         
         facebookImage.image = facebookImage.image?.withRenderingMode(.alwaysTemplate)
         facebookImage.tintColor = UIColor.init(red: 250.0/255.0, green: 145.0/255.0, blue: 150.0/255.0, alpha: 1.0)
         
-        googleImage.image = googleImage.image?.withRenderingMode(.alwaysTemplate)
-        googleImage.tintColor = UIColor.init(red: 250.0/255.0, green: 145.0/255.0, blue: 150.0/255.0, alpha: 1.0)
-        
+//        googleImage.image = googleImage.image?.withRenderingMode(.alwaysTemplate)
+//        googleImage.tintColor = UIColor.init(red: 250.0/255.0, green: 145.0/255.0, blue: 150.0/255.0, alpha: 1.0)
         
     }
     
@@ -43,7 +46,7 @@ class LoginViewController: UIViewController {
         fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
             
             guard error == nil else {
-                print(error?.localizedDescription ?? "")
+                print("Failed to login: \(error?.localizedDescription)")
                 return
             }
             
@@ -52,11 +55,36 @@ class LoginViewController: UIViewController {
                 return
             }
             
-            guard let tokenString = result?.token.tokenString else {
+            guard let accessToken = result?.token.tokenString else {
+                print("Failed to get access token")
                 return
             }
             
-            print("ID:\(String(describing: result?.token.appID)), Token:\(String(describing: tokenString))")
+//            print("ID:\(String(describing: result?.token.appID)), Token:\(String(describing: accessToken))")
+            
+            let credential = FacebookAuthProvider.credential(withAccessToken: accessToken)
+            
+            Auth.auth().signInAndRetrieveData(with: credential, completion: { (result, error) in
+                
+                if let error = error {
+                    
+                    print("Login error: \(error.localizedDescription)")
+                    let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(okAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                }
+                
+                if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MainPage") {
+                    
+                    UIApplication.shared.keyWindow?.rootViewController = viewController
+                    self.dismiss(animated: true, completion: nil)
+                    
+                }
+                
+            })
+
 //            print(Auth.auth().currentUser?.displayName)
 //            print(Auth.auth().currentUser?.photoURL?.absoluteString)
             
